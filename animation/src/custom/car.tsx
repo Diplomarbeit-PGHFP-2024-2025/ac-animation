a63010be65bb9c5bed5bec7a672b903cebbc6f46
jonas <jonaspfeifer@drei.at>
2025-04-09 17:32:01 +0200

    color changing batterie

--- animation/src/custom/car.tsx
+++ animation/src/custom/car.tsx
@@ -1,12 +1,51 @@
-import {NodeProps, Node, Img} from "@motion-canvas/2d";
-import {createEffect, createRef, createSignal, SimpleSignal, Vector2} from "@motion-canvas/core";
+import {NodeProps, Node, Img, initial, signal, computed, Circle, Layout, Rect} from "@motion-canvas/2d";
+import {
+    createEffect,
+    createRef,
+    createSignal, Reference,
+    SignalValue,
+    SimpleSignal,
+    useLogger,
+    Vector2
+} from "@motion-canvas/core";
 
-export const TIRE_DIAMETER = 100;
+export const TIRE_DIAMETER = 70;
+export const FRONT_TIRE_OFFSET = new Vector2(138, 71)
+export const BACK_TIRE_OFFSET = new Vector2(-181, 71)
+
+export const BATTERIE_OFFSET = new Vector2(-15, 30)
+export const BATTERIE_WIDTH = 125
+export const INTERNAL_BATTERIE_WIDTH = BATTERIE_WIDTH * 0.75
+export const LEFT_INTERNAL_BATTERIE_OFFSET = -BATTERIE_WIDTH / 2 + INTERNAL_BATTERIE_WIDTH / 2 + BATTERIE_WIDTH * 0.1
+export const BATTERIE_BAR_COUNT = 5
+export const BATTERIE_BAR_GAP = 8
+export const BATTERIE_BAR_WIDTH = (INTERNAL_BATTERIE_WIDTH - (BATTERIE_BAR_COUNT - 1) * BATTERIE_BAR_GAP) / BATTERIE_BAR_COUNT
+export const INTERNAL_BATTERIE_HEIGHT_FACTOR = 0.65
 
 export interface CarProps extends NodeProps {
+    flipped?: SignalValue<boolean>
+    soc?: SignalValue<number>
 }
 
 export class Car extends Node {
+    @initial(false)
+    @signal()
+    public declare readonly flipped: SimpleSignal<boolean>
+
+    @initial(1)
+    @signal()
+    public declare readonly soc: SimpleSignal<number>
+
+    private batterieColor = createSignal(() => {
+        if (this.soc() < 0.35) {
+            return "red"
+        } else if (this.soc() < 0.65) {
+            return "orange"
+        } else {
+            return "green"
+        }
+    })
+
     public constructor(props?: CarProps) {
         super({
             ...props,
@@ -15,13 +54,64 @@ export class Car extends Node {
         const carParent = createRef<Node>();
 
         const tireRotation = this.computedTireRotation();
+        const scale = this.createFlippedScale();
+        const socLevel = this.computedSocLevel();
 
-        this.add(<Node ref={carParent} position={this.position}>
-            <Img src={'resources/tire.svg'} position={[-150, 0]} width={TIRE_DIAMETER} height={TIRE_DIAMETER}
+        const barContainer = createRef<Layout>();
+        const batterie = createRef<Img>()
+
+        this.add(<Node ref={carParent} position={this.position} scaleX={scale}>
+            <Img src={'resources/car_body.svg'}></Img>
+            <Img ref={batterie} src={'resources/batterie.svg'} position={BATTERIE_OFFSET} width={BATTERIE_WIDTH}>
+                <Layout layout
+                        ref={barContainer}
+                        justifyContent={'start'}
+                        alignItems={'center'}
+                        gap={BATTERIE_BAR_GAP}
+                        width={INTERNAL_BATTERIE_WIDTH}
+                        position={[LEFT_INTERNAL_BATTERIE_OFFSET, 0]}>
+                </Layout>
+            </Img>
+            <Img src={'resources/tire.svg'} position={[BACK_TIRE_OFFSET.x, BACK_TIRE_OFFSET.y]} width={TIRE_DIAMETER}
+                 height={TIRE_DIAMETER}
                  rotation={tireRotation}/>
-            <Img src={'resources/tire.svg'} position={[150, 0]} width={TIRE_DIAMETER} height={TIRE_DIAMETER}
+            <Img src={'resources/tire.svg'} position={[FRONT_TIRE_OFFSET.x, FRONT_TIRE_OFFSET.y]} width={TIRE_DIAMETER}
+                 height={TIRE_DIAMETER}
                  rotation={tireRotation}/>
         </Node>)
+
+        const bars: Rect[] = []
+        createEffect(() => {
+            const currentAmount = bars.length;
+            const barCountChange = socLevel() - currentAmount;
+
+            for (let i = 0; i < barCountChange; i++) {
+                const bar = (<Rect
+                    width={BATTERIE_BAR_WIDTH}
+                    height={() => batterie().height() * INTERNAL_BATTERIE_HEIGHT_FACTOR}
+                    fill={this.batterieColor}
+                />) as Rect;
+
+                bars.push(bar);
+                barContainer().add(bar);
+            }
+
+            for (let i = 0; i < -barCountChange; i++) {
+                bars.pop().remove();
+            }
+        })
+    }
+
+    private createFlippedScale(): SimpleSignal<number> {
+        const carScale = createSignal<number>(1)
+        createEffect(() => {
+            if (this.flipped()) {
+                carScale(-1)
+            } else {
+                carScale(1)
+            }
+        })
+        return carScale;
     }
 
     private computedTireRotation(): SimpleSignal<number> {
@@ -30,8 +120,7 @@ export class Car extends Node {
         let lastPosition = this.position();
         createEffect(() => {
             const currentPosition = this.position();
-
-            const distanceTraveled = Math.sign(currentPosition.sub(lastPosition).x) * currentPosition.sub(lastPosition).magnitude;
+            const distanceTraveled = currentPosition.sub(lastPosition).magnitude;
             const degreesTurned = distanceTraveled / (Math.PI * TIRE_DIAMETER) * 360;
             tireRotation(tireRotation() + degreesTurned);
 
@@ -39,5 +128,15 @@ export class Car extends Node {
         })
 
         return tireRotation;
+    }
+
+    private computedSocLevel(): SimpleSignal<number> {
+        return createSignal(() => {
+            return Math.round(this.soc() * BATTERIE_BAR_COUNT)
+        })
+    }
+
+    private createBatterieBar(batterie: Reference<Layout>) {
+
     }
 }
--- animation/src/scenes/example.tsx
+++ animation/src/scenes/example.tsx
@@ -1,8 +1,12 @@
-import {Circle, Img, Node, makeScene2D} from '@motion-canvas/2d';
-import {all, createEaseInBack, createRef, createRefMap, createSignal, waitFor} from '@motion-canvas/core';
+import {makeScene2D} from '@motion-canvas/2d';
+import {
+    all,
+    createRef,
+    linear,
+    tween,
+    waitFor
+} from '@motion-canvas/core';
 import {Car, TIRE_DIAMETER} from '../custom/car'
-
-const TIRE_SOURCE = "resources/tire.svg"
 
 export default makeScene2D(function* (view) {
 
@@ -13,10 +17,22 @@ export default makeScene2D(function* (view) {
     </>);
 
     yield* all(
-        car().position([TIRE_DIAMETER*Math.PI,0], 5)
+        car().position([TIRE_DIAMETER * Math.PI, 0], 2),
+        tween(2, value => {
+            car().soc(linear(value, 1, 0.6));
+        })
     )
-    yield* waitFor(1)
+
+    yield* waitFor(0.5)
+
+    car().flipped(true)
+
+    yield* waitFor(0.5)
+
     yield* all(
-        car().position([0,0], 5)
+        car().position([0, 0], 2),
+        tween(2, value => {
+            car().soc(linear(value, 0.6, 0.2));
+        })
     )
 });
