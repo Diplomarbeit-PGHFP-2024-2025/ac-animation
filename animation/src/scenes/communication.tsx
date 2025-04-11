ac84afea8e69f2d421ff55a3b531a75b123284a4
jonas <jonaspfeifer@drei.at>
2025-04-11 11:37:02 +0200

    added communication

--- animation/src/scenes/communication.tsx
+++ animation/src/scenes/communication.tsx
@@ -1,9 +1,9 @@
-import {makeScene2D, Img, Layout, Node, Rect, View2D} from '@motion-canvas/2d';
+import {makeScene2D, Img, Layout, Node, Line, Rect, View2D, Txt} from '@motion-canvas/2d';
 import {
     all,
     chain,
-    createRef,
-    Reference,
+    createRef, createSignal,
+    Reference, SimpleSignal,
     spawn,
     ThreadGenerator,
     useLogger,
@@ -18,35 +18,95 @@ interface Agent {
     rect: Reference<Rect>
 }
 
+interface MessagePath {
+    a: Vector2,
+    b: Vector2,
+    message: string
+    scale: SimpleSignal<number>,
+    left: boolean,
+}
+
 class CommunicationAnimation {
-    private MESSAGE_PATH = [
-        new Vector2(-400, -200), new Vector2(400, -200),
-        new Vector2(400, -50), new Vector2(-400, -50),
-        new Vector2(-400, 100), new Vector2(400, 100),
-        new Vector2(400, 250), new Vector2(-400, 250),
+    private REGISTRY_MESSAGE_PATH: MessagePath[] = [
+        {
+            a: new Vector2(-350, -150),
+            b: new Vector2(-100, -150),
+            message: "Nahe Stationen?",
+            scale: createSignal(0),
+            left: true
+        },
+        {
+            a: new Vector2(-100, -50),
+            b: new Vector2(-350, -50),
+            message: "Stationen",
+            scale: createSignal(0),
+            left: false
+        },
+    ]
+
+    private STATION_MESSAGE_PATH: MessagePath[] = [
+        {
+            a: new Vector2(-350, 50),
+            b: new Vector2(350, 50),
+            message: "Deine Parameter?",
+            scale: createSignal(0),
+            left: false
+        },
+        {a: new Vector2(350, 150), b: new Vector2(-350, 150), message: "Parameter", scale: createSignal(0), left: true},
+        {
+            a: new Vector2(-350, 250),
+            b: new Vector2(350, 250),
+            message: "Reservieren?",
+            scale: createSignal(0),
+            left: false
+        },
+        {a: new Vector2(350, 350), b: new Vector2(-350, 350), message: "Ok", scale: createSignal(0), left: true},
     ]
 
     private readonly scene: Node;
 
     private readonly layout = createRef<Layout>();
-    private readonly message = createRef<Message>();
+    private readonly message = createRef<Node>();
     private agents: Agent[] = []
+    private readonly messageText = createSignal<string>("abc")
+    private lines: Reference<Line>[] = []
 
     constructor() {
-        this.scene = <>
-            <Layout ref={this.layout} gap={800} width={1000} alignContent={'center'} layout></Layout>
-            <Message ref={this.message} scale={0} width={75} position={[500, 500]}/>
-        </>
+        this.scene = <Node>
+            <Layout ref={this.layout} width={1000} justifyContent={'space-between'} layout></Layout>
+            <Node ref={this.message} scale={0} position={[500, 500]} zIndex={2}>
+                <Message width={60}/>
+            </Node>
+        </Node>
 
-        this.addAgent("resources/agent_body.svg")
+        this.addAgent("resources/agent.svg")
+        this.addAgent("resources/registry_body.svg")
         this.addAgent("resources/station_body.svg")
+
+        this.addText()
     }
 
     * animate(view: View2D): ThreadGenerator {
         view.add(this.scene)
 
-        yield* this.appearAgents()
-        yield* this.animateMessage()
+        yield* all(
+            this.appearAgent(this.agents[0]),
+            this.appearAgent(this.agents[1]),
+            this.appearAgent(this.agents[2]),
+        )
+
+        yield* this.animateMessage(this.REGISTRY_MESSAGE_PATH)
+
+        yield* this.animateMessage(this.STATION_MESSAGE_PATH)
+
+        yield* waitFor(0.5)
+        yield* all(
+            this.disappearAgent(this.agents[0]),
+            this.disappearAgent(this.agents[1]),
+            this.disappearAgent(this.agents[2]),
+            this.disappearLines(),
+            this.disappearText()
+        )
 
         yield* waitFor(10)
     }
@@ -55,7 +115,7 @@ class CommunicationAnimation {
         const agent: Agent = {img: createRef<Img>(), rect: createRef<Rect>()};
 
         this.layout().add(
-            <Layout height={900} gap={20} alignItems={'center'} justifyContent={'center'} direction={'column'} layout>
+            <Layout height={1000} gap={20} alignItems={'center'} justifyContent={'center'} direction={'column'} layout>
                 <Img ref={agent.img} src={src} size={100} scale={0}/>
                 <Rect ref={agent.rect} width={10} height={0} radius={20} fill="#000"/>
             </Layout>
@@ -64,32 +124,81 @@ class CommunicationAnimation {
         this.agents.push(agent)
     }
 
-    * appearAgents(): ThreadGenerator {
-        const actions = this.agents.map((agent: Agent) => {
-            return all(
-                agent.img().scale(1, 1),
-                agent.rect().height(600, 1)
-            )
-        });
+    addText() {
+        for (let path of [...this.REGISTRY_MESSAGE_PATH, ...this.STATION_MESSAGE_PATH]) {
+            const position = path.b.add(path.a).div(new Vector2(2, 2)).add(new Vector2(0, -35));
 
+            if (position.x === 0) {
+                if (!path.left) {
+                    position.x = -150;
+                } else {
+                    position.x = 150;
+
+                }
+            }
+
+            useLogger().info("" + position)
+            this.scene.add(
+                <Txt scale={path.scale} zIndex={3} position={position} text={path.message} fontSize={25}/>
+            )
+        }
+    }
+
+    * appearAgent(agent: Agent): ThreadGenerator {
         yield* all(
-            ...actions
+            agent.img().scale(1, 1),
+            agent.rect().height(700, 1)
+        )
+    }
+
+    * disappearAgent(agent
+                     :
+                     Agent
+    ):
+        ThreadGenerator {
+        yield* all(
+            agent.img().scale(0, 1),
+            agent.rect().height(0, 1)
         );
     }
 
-    * animateMessage(): ThreadGenerator {
-        this.message().position(this.MESSAGE_PATH[0])
-        yield* this.message().scale(1,0.5)
-        yield* waitFor(0.5)
+    * animateMessage(path
+                     :
+                     MessagePath[]
+    ):
+        ThreadGenerator {
+        for (let i = 0; i < path.length; i++) {
+            this.messageText(path[i].message)
+            this.message().position(path[i].a)
+            yield* this.message().scale(1, 0.5)
 
-        for (let i = 1; i < this.MESSAGE_PATH.length; i+=2) {
-            yield* this.message().position(this.MESSAGE_PATH[i], 1)
-            yield* this.message().scale(0,0.5)
-            if (i < this.MESSAGE_PATH.length - 2) {
-                this.message().position(this.MESSAGE_PATH[i + 1])
-                yield* this.message().scale(1,0.5)
-            }
+            const line = createRef<Line>();
+            this.scene.add(<Line ref={line} end={0} points={[path[i].a, path[i].b]} endArrow lineWidth={6}
+                                 arrowSize={20}
+                                 stroke={'black'}/>);
+            this.lines.push(line);
+            yield* all(
+                this.message().position(path[i].b, 1),
+                line().end(1, 1.1),
+                path[i].scale(1, 0.5)
+            )
+            yield* this.message().scale(0, 0.5)
         }
+    }
+
+    *disappearLines(): ThreadGenerator {
+        const actions = this.lines.map(line => line().end(0, 1))
+
+        yield* all(...actions)
+    }
+
+    *disappearText(): ThreadGenerator {
+        const actions = []
+        for (let path of [...this.REGISTRY_MESSAGE_PATH, ...this.STATION_MESSAGE_PATH]) {
+            actions.push(path.scale(0,1))
+        }
+
+        yield* all(...actions)
     }
 }
 
