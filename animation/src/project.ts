d9eabbdc9a22a04a7507c380801829def5b7c9ca
tobinio <Tobias.frischmann1@gmail.com>
2025-04-21 12:50:15 +0200

    add test audio

--- animation/src/project.ts
+++ animation/src/project.ts
@@ -1,9 +1,12 @@
 import {makeProject} from '@motion-canvas/core';
 
+import audio from '../resources/Audio.mp3';
+
 import car from './scenes/car?scene';
 import pathfinder from "./scenes/pathfinder?scene";
 import communication from "./scenes/communication?scene";
 
 export default makeProject({
     scenes: [car, communication, pathfinder],
+    audio: audio
 });
