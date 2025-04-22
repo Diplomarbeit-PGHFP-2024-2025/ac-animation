496b79d3c3d963c091afa71dd703f853992f0e2c
tobinio <Tobias.frischmann1@gmail.com>
2025-04-22 15:50:25 +0200

    basic audio sync

--- animation/src/project.ts
+++ animation/src/project.ts
@@ -1,6 +1,6 @@
 import {makeProject} from '@motion-canvas/core';
 
-import audio from '../resources/Audio.mp3';
+import audio from '../resources/ac_animation_sound.mp3';
 
 import car from './scenes/car?scene';
 import pathfinder from "./scenes/pathfinder?scene";
@@ -9,5 +9,5 @@ import drive from "./scenes/drive?scene";
 
 export default makeProject({
     scenes: [car, communication, pathfinder, drive],
-    audio: audio
+    audio: audio,
 });
